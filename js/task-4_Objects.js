67acf1994760ecc6b7499aa805f555c5b9320124
VladislavShatruk <vladshatruk121@gmail.com>
2024-04-14 12:25:22 +0300

    Update task-4_Objects.js

--- js/task-4_Objects.js
+++ js/task-4_Objects.js
@@ -430,18 +430,18 @@
 
 
 
-const defaultSettings = {
-    theme: "light",
-    public: true,
-    withPassword: false,
-    minNumberOfQuestions: 10,
-    timePerQuestion: 60,
-  };
-  const overrideSettings = {
-    public: false,
-    withPassword: true,
-    timePerQuestion: 30,
-  };
+// const defaultSettings = {
+//     theme: "light",
+//     public: true,
+//     withPassword: false,
+//     minNumberOfQuestions: 10,
+//     timePerQuestion: 60,
+//   };
+//   const overrideSettings = {
+//     public: false,
+//     withPassword: true,
+//     timePerQuestion: 30,
+//   };
   
-  const finalSettings = {...defaultSettings, ...overrideSettings};
-console.table(finalSettings);  
+//   const finalSettings = {...defaultSettings, ...overrideSettings};
+// console.table(finalSettings);  
