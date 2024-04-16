67bfe27330219547e3ca4cd4375b5df8b804ab70
VladislavShatruk <vladshatruk121@gmail.com>
2024-04-16 22:06:15 +0300

    fgf

---
+++ js/task-6_OOP_classes.js
@@ -0,0 +1,43 @@
+// const customer = {
+// 	username: "Jacob"
+// };
+
+// const animal = { 
+// 	legs: 4 
+// };
+
+// const dog = Object.create(animal);
+// dog.name = "Mango";
+
+// console.log(dog); // { name: "Mango", [[Prototype]]: animal }
+
+// console.log(animal.isPrototypeOf(dog)); // true
+// console.log(dog.isPrototypeOf(animal)); // false
+// console.log(customer.isPrototypeOf(dog)); // false
+
+
+
+// const animal = { legs: 4 };
+// const dog = Object.create(animal);
+// dog.name = "Mango";
+
+// console.log(Object.keys(dog)); // ["name"]
+// console.log(Object.values(dog)); // ["Mango"]
+
+// for(const key of Object.keys(dog)) {
+// 	console.log(key); // "name"
+// }
+
+
+
+const objC = { c: "objC prop" };
+
+const objB = Object.create(objC);
+objB.b = "objB prop";
+
+const objA = Object.create(objB);
+objA.a = "objA prop";
+
+console.log(objA); // { a: "objA prop", [[Prototype]]: objB }
+console.log(objB); // { b: "objB prop", [[Prototype]]: objC }
+console.log(objC); // { c: "objC prop", [[Prototype]]: Object }
