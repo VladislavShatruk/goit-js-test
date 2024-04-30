7ee6088c55986e19aa837e04d59e6f4298f30e32
VladislavShatruk <vladshatruk121@gmail.com>
2024-04-30 09:00:26 +0300

    a

--- js/task-7_DOM_events.js
+++ js/task-7_DOM_events.js
@@ -14,3 +14,13 @@
 // console.log(box);
 
 // document.body.append(heading,image,box);
+
+
+
+const button = document.querySelector(".my-button");
+
+const handleClick = ( ) => { 
+    console.log("The button was pressed and now the next image will appear");
+ };
+ 
+ button.addEventListener("click, handleClick");
