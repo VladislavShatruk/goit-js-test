251d15b867a0b04a540ec4cd34032375535b2205
VladislavShatruk <vladshatruk121@gmail.com>
2024-05-28 22:44:49 +0300

    Update task-10_asynchr_JS+promis.js

--- js/task-10_asynchr_JS+promis.js
+++ js/task-10_asynchr_JS+promis.js
@@ -111,3 +111,65 @@
 // console.log(date); // "Wed May 16 2040 14:50:00 GMT+0300"
 
 
+// const isSuccess = true;
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (isSuccess) {
+//       resolve("Success! Value passed to resolve function");
+//     } else {
+//       reject("Error! Error passed to reject function");
+//     }
+//   }, 2000);
+// });
+
+// console.log(promise); // Об'єкт промісу
+
+
+
+// const isSuccess = true;
+
+// // Create promise
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (isSuccess) {
+//       resolve("Success! Value passed to resolve function");
+//     } else {
+//       reject("Error! Error passed to reject function");
+//     }
+//   }, 2000);
+// });
+
+// // Registering promise callbacks
+// promise
+// .then(value => console.log(value)) // "Success! Value passed to resolve function"
+// .catch(error => console.log(error)) // "Error! Error passed to reject function"
+// .finally(() => console.log("Promise settled")); // "Promise settled"
+
+
+
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(5);
+    }, 2000);
+  });
+  
+  promise
+    .then(value => {
+      console.log(value); // 5
+      return value * 2;
+    })
+    .then(value => {
+      console.log(value); // 10
+      return value * 3;
+    })
+    .then(value => {
+      console.log(value); // 30
+    })
+    .catch(error => {
+      console.log(error);
+    })
+    .finally(() => {
+      console.log("finally");
+    });
+  
